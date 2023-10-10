0749bee9ec817b2d82fa517af89f0890ddaae21d
Aimoza <admin@chenzhihao.cc>
2023-10-10 21:02:27 +0800

    upd(web):update main.js, enable ElementPlus

--- typress-web/src/main.js
+++ typress-web/src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import {createApp} from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import router from './router'; // 导入路由配置
 import axios from 'axios';
 
@@ -10,4 +12,4 @@ axios.defaults.baseURL = 'http://127.0.0.1:8080/';
 // 设置通用的请求头
 axios.defaults.headers.common['session_id'] = localStorage.getItem('session_id');
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(ElementPlus).use(router).mount('#app');
