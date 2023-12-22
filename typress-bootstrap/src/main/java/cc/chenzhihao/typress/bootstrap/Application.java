845b0626c353d7bf76821292f296770d40e27579
jacksonchenzhihao <jacksonchenzhihao@didiglobal.com>
2023-12-22 17:14:08 +0800

    upd #7

--- typress-bootstrap/src/main/java/cc/chenzhihao/typress/bootstrap/Application.java
+++ typress-bootstrap/src/main/java/cc/chenzhihao/typress/bootstrap/Application.java
@@ -3,6 +3,7 @@ package cc.chenzhihao.typress.bootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 应用程序启动入口
@@ -12,6 +13,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
  */
 @SpringBootApplication
 @EnableConfigurationProperties
+@ComponentScan("cc.chenzhihao.typress")
 public class Application {
 
     public static void main(String[] args) {
